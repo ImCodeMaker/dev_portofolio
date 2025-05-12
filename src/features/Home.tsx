e5b7bd3f4912910501f9f72e46bf10a0a25a7288
ImCodeMaker <xxjoshi2006@gmail.com>
2025-05-12 14:42:48 -0400

    Fixed Apos

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="w-screen h-screen bg-white flex items-center justify-center">
         <div className="flex flex-col justify-center items-center">
           <h2 className="font-bold tracking-wide text-4xl drop-shadow-lg hover:drop-shadow-2xl text-black">
-            Hello! I'm Teudy!
+            Hello! I&apos;m Teudy!
           </h2>
           <div className="flex items-center justify-center text-center mt-2 drop-shadow-2xl">
             <p className="text-black font-normal tracking wide hyphens-manual ">
