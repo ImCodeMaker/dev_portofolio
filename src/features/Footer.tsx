09d7e85dae26279512e0a3b3bd4a37848bebf3f3
ImCodeMaker <xxjoshi2006@gmail.com>
2025-05-13 13:45:57 -0400

    bug fixing

--- src/features/Footer.tsx
+++ src/features/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full min-h-screen  border-t border-zinc-200 bg-white py-6">
+    <footer className="w-full border-t border-zinc-200 bg-white py-6">
       <div className="max-w-4xl mx-auto px-4 text-center">
         <p className="text-black text-sm md:text-base font-light italic">
           © 2025 Teudy Joshua Encarnacion. All rights reserved.
